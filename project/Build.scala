9e2c22f743933abde8a4f2d0c10a4aa1d1d3c979
Thomas Alexandre <thomas_alexandre@yahoo.com>
2013-04-03 18:14:27 +0200

    upgraded sl4j

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object ApplicationBuild extends Build {
     jdbc,
     anorm,
     "com.typesafe.slick" % "slick_2.10" % "1.0.0",
-    "org.slf4j" % "slf4j-nop" % "1.6.4",
+    "org.slf4j" % "slf4j-api" % "1.7.3",
     "com.h2database" % "h2" % "1.3.166",
     "org.xerial" % "sqlite-jdbc" % "3.6.20",
     "org.apache.derby" % "derby" % "10.6.1.0",
